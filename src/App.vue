4906f63c225d053e8cb1dbfa465f4494ea048774
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-03-16 10:04:59 +0300

    styleclass and ripple demos added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h5>Ripple</h5>
+  <div class="card-container flex">
+      <div class="card primary-box p-ripple" v-ripple>Default</div>
+  </div>
+
+
+  <h5>StyleClass</h5>
+  <Button label="Show" class="mr-2" v-styleclass="{ selector: '.box', enterClass: 'hidden', enterActiveClass: 'my-fadein' }" />
+  <Button label="Hide" v-styleclass="{ selector: '.box', leaveActiveClass: 'my-fadeout', leaveToClass: 'hidden' }" />
+  <div class="box hidden">Content</div>
 </template>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,5 +24,64 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+ 
+}
+
+.card-container {
+    background-color: var(--surface-300);
+    padding: 1rem;
+
+    .card {
+        width: 75px;
+        height: 75px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-right: 1rem;
+        user-select: none;
+        padding: 0;
+        border-radius: 10px;
+        background-color: var(--surface-0);
+
+        &.primary-box {
+            background-color: var(--primary-color);
+            padding: 0;
+            color: var(--primary-color-text);
+        }
+    }
+}
+
+.box {
+    background-color: var(--green-500);
+    color: #ffffff;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    border-radius: 4px;
+    margin-top: 1rem;
+    font-weight: bold;
+    box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
+}
+
+@keyframes my-fadein {
+    0%   { opacity: 0; }
+    100% { opacity: 1; }
+}
+
+@keyframes my-fadeout {
+    0%   { opacity: 1; }
+    100% { opacity: 0; }
+}
+
+.my-fadein {
+    animation: my-fadein 150ms linear;
+}
+        
+.my-fadeout {
+    animation: my-fadeout 150ms linear;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,18 @@ import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
 import InputNumber from 'primevue/inputnumber';
+import StyleClass from 'primevue/styleclass';
+import Ripple from 'primevue/ripple';
 
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
+import 'primeflex/primeflex.css';
 
 const app = createApp(App);
 
-app.use(PrimeVue);
+app.use(PrimeVue, { ripple: true });
+app.directive('styleclass', StyleClass);
+app.directive('ripple', Ripple);
 
 app.component('Button', Button);
 app.component('InputNumber', InputNumber);
