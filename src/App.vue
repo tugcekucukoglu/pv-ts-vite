5189fb11e6b34a2bee4dabc98135c900468f4ddb
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-06-09 09:18:22 +0300

    defineExpose usage for directives

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,25 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import vRipple from 'primevue/ripple';
-import { useToast } from "primevue/usetoast";
+import vStyleclass from 'primevue/styleclass';
+import { usePrimeVue } from "primevue/config";
 
-const toast = useToast();
-
-function showSuccess() {
-    toast.add({
-        severity: "success",
-        summary: "Success Message",
-        detail: "Message Content",
-        life: 3000,
-    });
-}
+const $primevue = usePrimeVue();
+defineExpose({
+    $primevue
+})
 </script>
 
 <template>
-    <Toast />
-    <Button label="Success" class="p-button-success" @click="showSuccess" />
-
     <h5>Ripple</h5>
     <div class="card-container flex">
         <div class="card primary-box p-ripple" v-ripple>Default</div>
     </div>
+
+    <h5>StyleClass Animations</h5>
+    <Button label="Show" class="mr-2" v-styleclass="{ selector: '.box', enterClass: 'hidden', enterActiveClass: 'my-fadein' }" />
+    <Button label="Hide" v-styleclass="{ selector: '.box', leaveActiveClass: 'my-fadeout', leaveToClass: 'hidden' }" />
+    <div class="box hidden">Content</div>
 </template>
 
 <style lang="scss" scoped>
@@ -49,22 +46,6 @@ function showSuccess() {
             color: var(--primary-color-text);
         }
     }
-}
-
-.box {
-    background-color: var(--green-500);
-    color: #ffffff;
-    width: 100px;
-    height: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    border-radius: 4px;
-    margin-top: 1rem;
-    font-weight: bold;
-    box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
 }
 
 @keyframes my-fadein {
